e1b81eeb40c73d4cfa6c9cf0dca516d9d87a50f7
Alexander Terentiev <a.terentev@examus.net>
2022-11-14 19:09:19 +0500

    add tests

--- polls/views.py
+++ polls/views.py
@@ -2,26 +2,39 @@ from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-
+from django.views import generic
+from django.utils import timezone
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, "polls/index.html", context)
+def get_queryset_pub_date_lte_now():
+    return Question.objects.filter(
+            pub_date__lte=timezone.now(),
+        )
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return get_queryset_pub_date_lte_now().order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return get_queryset_pub_date_lte_now()
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        return get_queryset_pub_date_lte_now()
 
 
 def vote(request, question_id):
